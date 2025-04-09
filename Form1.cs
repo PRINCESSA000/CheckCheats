2458f215000bce33a16dc7c12db17a3802733d92
Colizeum234 <Colizeum234@ADMIN-PC>
2025-04-09 22:06:39 +0300

    кнопочка с обезьяной

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace CheckCheats
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ты павиан");
+        }
     }
 }
